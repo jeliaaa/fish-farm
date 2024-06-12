78f0361bacde473407b9c92f5be77ecae77bf8f7
jeliaaa <julia.aleqsandre@gmail.com>
2024-06-12 12:34:25 +0400

    k

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -5,32 +5,16 @@ import React from 'react';
 const ContactUs = () => {
     return (
         <div style={styles.container}>
-            <h2 style={styles.header}>Contact Us</h2>
+            <h2 style={styles.header}>კონტაქტები</h2>
 
             <div style={styles.contactMethods}>
                 <div style={styles.contactMethod}>
-                    <h3>Email</h3>
-                    <p>Email us at: <a href="mailto:starfish.info.btm@gmail.com">your.email@example.com</a></p>
+                    <h3>ელ.ფოსტა</h3>
+                    <p>დაგვეონტაქტეთ: <a href="mailto:starfish.info.btm@gmail.com">starfish.info.btm@gmail.com</a></p>
                 </div>
-
                 <div style={styles.contactMethod}>
-                    <h3>LinkedIn</h3>
-                    <p>Connect with me on <a style={{textDecoration:'underline'}} href="https://www.linkedin.com/in/your-linkedin" target="_blank" rel="noopener noreferrer">LinkedIn</a> <i className='fa-brands fa-linkedin'></i></p>
-                </div>
-
-                <div style={styles.contactMethod}>
-                    <h3>Facebook</h3>
-                    <p>Find me on <a href="https://www.facebook.com/your.facebook" target="_blank" rel="noopener noreferrer" style={{textDecoration:'underline'}}>Facebook</a> <i className='fa-brands fa-facebook'></i></p>
-                </div>
-
-                <div style={styles.contactMethod}>
-                    <h3>Instagram</h3>
-                    <p>Follow me on <a style={{textDecoration:'underline'}} href="https://www.instagram.com/your.instagram" target="_blank" rel="noopener noreferrer">Instagram</a><i className='fa-brands fa-instagram'></i></p>
-                </div>
-
-                <div style={styles.contactMethod}>
-                    <h3>Mobile Phone</h3>
-                    <p>Call or message me at: <a href="tel:+123456789">+1 (234) 567-89</a></p>
+                    <h3>მობილური</h3>
+                    <p>დაგვირეკეთ ან მოგვწერეთ: <a href="tel:+995579166280">+995 579 16 62 80</a></p>
                 </div>
             </div>
         </div>
@@ -41,23 +25,24 @@ const styles = {
     container: {
         width: '100%',
         margin: 'auto',
-        paddingTop:'30px',
+        paddingTop: '30px',
         textAlign: 'center',
         display: 'flex',
         flexDirection: 'column'
     },
     header: {
         color: '#333',
+        textAlign: 'center'
     },
     contactMethods: {
         display: 'flex',
         flexDirection: 'column',
-        alignItems:'center',
+        alignItems: 'center',
         marginTop: '20px',
     },
     contactMethod: {
         flex: '1',
-        width:'60%',
+        width: '60%',
         padding: '20px',
         border: '1px solid #ccc',
         borderRadius: '8px',
